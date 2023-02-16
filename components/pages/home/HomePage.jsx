3ff8c97285550390b568b03ec81d91a6cc45b9ba
nathanelcorpuz <nathanelcorpuz@gmail.com>
2023-02-16 13:51:17 +0800

    search implementation (frontend)

--- components/pages/home/HomePage.jsx
+++ components/pages/home/HomePage.jsx
@@ -2,8 +2,24 @@ import styles from "./HomePage.module.css";
 import ArticlePreviewSection from "@/components/common/article/article-preview/ArticlePreviewSection";
 import ShowMoreBtn from "@/components/common/show-more-btn/ShowMoreBtn";
 import Article from "../../common/article/article-preview/ArticlePreview";
+import { useState } from "react";
 
 export default function HomePage({ articles }) {
+  const [moreArticles, setMoreArticles] = useState([]);
+
+  let lastArticleId;
+
+  if (articles) {
+    const moreArticlesLastId = moreArticles.length
+      ? moreArticles[moreArticles.length - 1].id
+      : "";
+
+    const initialAllLastId =
+      articles.initialAll[articles.initialAll.length - 1].id;
+
+    lastArticleId = moreArticlesLastId || initialAllLastId;
+  }
+
   return (
     <>
       <h1 className={styles.h1}>Nathanel Corpuz Blog</h1>
@@ -18,12 +34,25 @@ export default function HomePage({ articles }) {
       </ArticlePreviewSection>
       <ArticlePreviewSection view="all">
         <h2 className={styles.all_title}>All</h2>
-        {articles.all.map((article) =>
-          article.id === "article7" ? null : (
-            <Article key={article.id} article={article} />
-          )
-        )}
-        <ShowMoreBtn />
+        {articles.initialAll.map((article) => (
+          <Article key={article.slug} article={article} />
+        ))}
+        {moreArticles.map((article) => (
+          <Article key={article.slug} article={article} />
+        ))}
+        <ShowMoreBtn
+          onClick={async () => {
+            const res = await fetch(
+              `http://localhost:3000/api/articles/show-more-all/${lastArticleId}`
+            );
+            const nextThreeArticles = await res.json();
+
+            setMoreArticles((prevState) => [
+              ...prevState,
+              ...nextThreeArticles,
+            ]);
+          }}
+        />
       </ArticlePreviewSection>
     </>
   );
