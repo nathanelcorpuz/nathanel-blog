45d8678f0643ca88e63cdbd5158cae722769e97f
nathanelcorpuz <nathanelcorpuz@gmail.com>
2023-02-16 10:49:30 +0800

    category page responsive

--- components/pages/category/CategoryPage.jsx
+++ components/pages/category/CategoryPage.jsx
@@ -1,7 +1,9 @@
+import styles from "./CategoryPage.module.css";
 import ArticlePreview from "@/components/common/article/article-preview/ArticlePreview";
 import ArticlePreviewSection from "@/components/common/article/article-preview/ArticlePreviewSection";
 import { capitalize } from "@/lib/utils/capitalize";
 import Head from "next/head";
+import ShowMoreBtn from "@/components/common/show-more-btn/ShowMoreBtn";
 
 export default function CategoryPage({ articles, category }) {
   return (
@@ -13,17 +15,15 @@ export default function CategoryPage({ articles, category }) {
             : `${capitalize(category)} - Blog - Nathanel Corpuz`}
         </title>
       </Head>
-      <h1 style={{ textTransform: "capitalize", padding: "0 20px" }}>
-        {!articles ? "Loading..." : category}
-      </h1>
+      <h1 className={styles.h1}>{!articles ? "Loading..." : category}</h1>
       {articles && (
         <>
-          <ArticlePreviewSection>
+          <ArticlePreviewSection view="category">
             {articles.map((article) => (
               <ArticlePreview key={article.id} article={article} />
             ))}
           </ArticlePreviewSection>
-          <button style={{ margin: "0 20px" }}>Show more</button>
+          <ShowMoreBtn style={{ margin: "0 auto" }} />
         </>
       )}
     </>
