bec4ed76e5b7e4e5d930c71a2315fe27bd399478
nathanelcorpuz <nathanelcorpuz@gmail.com>
2023-02-22 00:53:31 +0800

    private helpers

--- pages/api/admin/new-article.js
+++ pages/api/admin/new-article.js
@@ -1,12 +1,12 @@
-import jwt from "jsonwebtoken";
 import connectMongo from "@/helpers/connectMongo";
+import verifyToken from "@/helpers/private/verifyToken";
 
 export default async function handler(req, res) {
   await connectMongo();
 
   const token = req.headers.authorization.slice(7);
 
-  const { isAdmin } = jwt.verify(token, process.env.ADMIN_SECRET);
+  const isAdmin = verifyToken(token);
 
   if (!isAdmin) {
     res.status(401).send("Unauthorized access");
