400d3fcfcf5f0b1849e86bbb7822b5b64d5543ee
nathanelcorpuz <nathanelcorpuz@gmail.com>
2023-02-22 16:50:17 +0800

    admin layout

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,3 +1,14 @@
+import AdminLayout from "@/components/layout/admin/AdminLayout";
+import Layout from "@/components/layout/main/Layout";
+
 export default function Admin() {
   return <h1>Admin page</h1>;
 }
+
+Admin.getLayout = function getLayout(page) {
+  return (
+    <Layout>
+      <AdminLayout>{page}</AdminLayout>
+    </Layout>
+  );
+};
