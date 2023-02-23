581a9ea41115cd10f802c073ceff228ae31f51d0
nathanelcorpuz <nathanelcorpuz@gmail.com>
2023-02-23 18:19:28 +0800

    add paragraph refactor

--- lib/hooks/useNewArticleReducer.jsx
+++ lib/hooks/useNewArticleReducer.jsx
@@ -1,6 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 import { useReducer } from "react";
 import findNestedObj from "../utils/findNestedObj";
+import addParagraph from "../utils/addParagraph";
 
 const initialState = {
   image: {
@@ -114,35 +115,7 @@ function reducer(state, action) {
     }
     case "add_paragraph": {
       const stateClone = JSON.parse(JSON.stringify(state));
-      if (action.location === "heading2") {
-        const targetSection = findNestedObj(stateClone, "id", action.sectionId);
-        targetSection.content.paragraphs.push(action.newParagraph);
-        const targetSectionIndex = stateClone.sections.findIndex(
-          (section) => section.id === targetSection.id
-        );
-        stateClone.sections.splice(targetSectionIndex, 1, targetSection);
-        return {
-          ...stateClone,
-        };
-      }
-
-      if (action.location === "heading3") {
-        const targetSection = findNestedObj(stateClone, "id", action.sectionId);
-        const targetItem = findNestedObj(stateClone, "id", action.itemId);
-        targetItem.paragraphs.push(action.newParagraph);
-        const targetSectionIndex = stateClone.sections.findIndex(
-          (section) => section.id === targetSection.id
-        );
-        const targetItemIndex = targetSection.content.items.findIndex(
-          (item) => item.id === targetItem.id
-        );
-        targetSection.content.items.splice(targetItemIndex, 1, targetItem);
-        stateClone.sections.splice(targetSectionIndex, 1, targetSection);
-        return {
-          ...stateClone,
-        };
-      }
-      return stateClone;
+      return addParagraph(stateClone, action);
     }
     case "add_items": {
     }
